88dba1aa4e74fbb2531f4660f1e3d16ee297c034
mpresecan <m_presecan@yahoo.com>
2024-11-13 20:04:07 +0100

    Quartz sync: Nov 13, 2024, 8:04 PM

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -19,7 +19,7 @@ export default (() => {
 
     const iconPath = 'https://forgotten-pillar.s3.us-east-2.amazonaws.com/fp-notes-logo.svg'
 
-    const ogImagePath = fileData?.frontmatter?.title ? `https://forgottenpillar.com/api/og-notes?title=${fileData.frontmatter?.title}` : 'https://forgotten-pillar.s3.us-east-2.amazonaws.com/og-notes.png'
+    const ogImagePath = fileData?.frontmatter?.title ? `https://forgottenpillar.com/api/og-notes?title=${encodeURIComponent(fileData.frontmatter?.title)}` : 'https://forgotten-pillar.s3.us-east-2.amazonaws.com/og-notes.png'
 
     return (
       <head>
