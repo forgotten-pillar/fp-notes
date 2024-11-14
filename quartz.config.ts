894f2300f4245deec1cec2222e3b502d5a90d74c
mpresecan <m_presecan@yahoo.com>
2024-11-14 22:12:17 +0100

    Quartz sync: Nov 14, 2024, 10:12 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -89,7 +89,7 @@ const config: QuartzConfig = {
       Plugin.Assets(),
       Plugin.Static(),
       Plugin.NotFoundPage(),
-      Plugin.Broadcaster(),
+      // Plugin.Broadcaster(),
     ],
   },
 }
