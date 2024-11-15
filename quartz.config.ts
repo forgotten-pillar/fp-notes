838c8431c37363c0b16927e9c1b64ffa5b64407c
mpresecan <m_presecan@yahoo.com>
2024-11-15 17:17:11 +0100

    Quartz sync: Nov 15, 2024, 5:17 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -60,7 +60,7 @@ const config: QuartzConfig = {
     transformers: [
       Plugin.FrontMatter(),
       Plugin.CreatedModifiedDate({
-        priority: ["frontmatter", "filesystem", "git"],
+        priority: ["frontmatter", "git", "filesystem"],
       }),
       Plugin.SyntaxHighlighting({
         theme: {
