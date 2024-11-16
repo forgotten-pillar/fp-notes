4ec070a65afb04385c922c1da903a98b0f911363
mpresecan <m_presecan@yahoo.com>
2024-11-16 19:51:35 +0100

    Quartz sync: Nov 16, 2024, 7:51 PM

--- quartz/components/HamburgerMenu.tsx
+++ quartz/components/HamburgerMenu.tsx
@@ -5,7 +5,8 @@ import hamburgerScript from './scripts/hamburdermenu.inline'
 
 function HamburgerMenu({ displayClass }: QuartzComponentProps) {
   return <div id='hamburger-menu' class={classNames(displayClass, "hamburder-menu")}>
-    <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-menu"><line x1="4" x2="20" y1="12" y2="12"/><line x1="4" x2="20" y1="6" y2="6"/><line x1="4" x2="20" y1="18" y2="18"/></svg>
+    <svg id="menu-icon" class="lucide lucide-menu" xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="4" x2="20" y1="12" y2="12"/><line x1="4" x2="20" y1="6" y2="6"/><line x1="4" x2="20" y1="18" y2="18"/></svg>
+    <svg id="close-icon" style="display: none" class="lucide lucide-x" xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>
   </div>
 }
 
--- quartz/components/scripts/hamburdermenu.inline.ts
+++ quartz/components/scripts/hamburdermenu.inline.ts
@@ -5,15 +5,25 @@ const toggleExplorer = () => {
     }
 
     const articleEl = document.getElementById('center-content');
-    if(articleEl) {
+    if (articleEl) {
         articleEl.classList.toggle('desktop-only');
     }
-}
+
+    // Toggle the icons
+    const menuIcon = document.getElementById('menu-icon');
+    const closeIcon = document.getElementById('close-icon');
+
+    if (menuIcon && closeIcon) {
+        if (menuIcon.style.display === 'none') {
+            menuIcon.style.display = 'block';
+            closeIcon.style.display = 'none';
+        } else {
+            menuIcon.style.display = 'none';
+            closeIcon.style.display = 'block';
+        }
+    }
+};
 
 document.getElementById('hamburger-menu')?.addEventListener('click', () => {
     toggleExplorer();
 });
-
-document.getElementById('close-explorer')?.addEventListener('click', () => {
-    toggleExplorer();
-})
