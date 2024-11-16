e6234faa892841c82a42dce67d163b490f327b1c
mpresecan <m_presecan@yahoo.com>
2024-11-16 17:40:58 +0100

    Quartz sync: Nov 16, 2024, 5:40 PM

--- quartz/components/ContentMeta.tsx
+++ quartz/components/ContentMeta.tsx
@@ -34,13 +34,13 @@ export default ((opts?: Partial<ContentMetaOptions>) => {
       if (fileData.dates) {
         const date = getDate(cfg, fileData)
         if(date) {
-          const icon = (<span><svg xmlns="http://www.w3.org/2000/svg" width="24" height="26" viewBox="0 0 34 10" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" icon-name="calendar-plus" class="lucide lucide-calendar-plus"><path d="M21 13V6a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h8"></path><line x1="16" y1="2" x2="16" y2="6"></line><line x1="8" y1="2" x2="8" y2="6"></line><line x1="3" y1="10" x2="21" y2="10"></line><line x1="19" y1="16" x2="19" y2="22"></line><line x1="16" y1="19" x2="22" y2="19"></line></svg><span>{formatDate(date, cfg.locale)}</span></span>)
+          const icon = (<><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" icon-name="calendar-plus" class="lucide lucide-calendar-plus"><path d="M21 13V6a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h8"></path><line x1="16" y1="2" x2="16" y2="6"></line><line x1="8" y1="2" x2="8" y2="6"></line><line x1="3" y1="10" x2="21" y2="10"></line><line x1="19" y1="16" x2="19" y2="22"></line><line x1="16" y1="19" x2="22" y2="19"></line></svg><span>{formatDate(date, cfg.locale)}</span></>)
           segments.push(icon)
         }
 
         if (fileData.dates.modified) {
           const modifiedDate = formatDate(fileData.dates.modified, cfg.locale)
-          const icon = (<span><svg xmlns="http://www.w3.org/2000/svg" width="24" height="26" viewBox="0 0 34 10" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" icon-name="calendar-clock" class="lucide lucide-calendar-clock"><path d="M21 7.5V6a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h3.5"></path><path d="M16 2v4"></path><path d="M8 2v4"></path><path d="M3 10h5"></path><path d="M17.5 17.5 16 16.25V14"></path><path d="M22 16a6 6 0 1 1-12 0 6 6 0 0 1 12 0Z"></path></svg><span>{modifiedDate}</span></span>)
+          const icon = (<><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" icon-name="calendar-clock" class="lucide lucide-calendar-clock"><path d="M21 7.5V6a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h3.5"></path><path d="M16 2v4"></path><path d="M8 2v4"></path><path d="M3 10h5"></path><path d="M17.5 17.5 16 16.25V14"></path><path d="M22 16a6 6 0 1 1-12 0 6 6 0 0 1 12 0Z"></path></svg><span>{modifiedDate}</span></>)
           segments.push(icon)
         }
       }
@@ -51,7 +51,7 @@ export default ((opts?: Partial<ContentMetaOptions>) => {
         const displayedTime = i18n(cfg.locale).components.contentMeta.readingTime({
           minutes: Math.ceil(minutes),
         })
-        const icon = (<span><svg xmlns="http://www.w3.org/2000/svg" width="24" height="26" viewBox="0 0 34 10" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-clock"><circle cx="12" cy="12" r="10"/><polyline points="12 6 12 12 16 14"/></svg><span>{displayedTime}</span></span>)
+        const icon = (<><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-clock"><circle cx="12" cy="12" r="10"/><polyline points="12 6 12 12 16 14"/></svg><span>{displayedTime}</span></>)
         segments.push(icon)
       }
 
@@ -59,11 +59,11 @@ export default ((opts?: Partial<ContentMetaOptions>) => {
       if(fileData.relativePath) {
         const path = fileData.relativePath.toString().split('/').map(encodeURIComponent).join('/');
         const editLink = `https://github.com/mpresecan/fp-notes/blob/v4/content/${path}`;
-        const editIcon = (<a href={editLink} target="_blank" rel="noopener noreferrer"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="26" viewBox="0 0 34 10" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-pencil"><path d="M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z"/><path d="m15 5 4 4"/></svg><span style={{fontWeight: 'normal'}}>edit</span></a>)
+        const editIcon = (<a class="meta-item" href={editLink} target="_blank" rel="noopener noreferrer"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-pencil"><path d="M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z"/><path d="m15 5 4 4"/></svg><span style={{fontWeight: 'normal'}}>edit</span></a>)
         segments.push(editIcon)
       }
 
-      const segmentsElements = segments.map((segment) => <span>{segment}</span>)
+      const segmentsElements = segments.map((segment) => <span class='meta-item'>{segment}</span>)
 
       return (
         <p show-comma={options.showComma} class={classNames(displayClass, "content-meta")}>
