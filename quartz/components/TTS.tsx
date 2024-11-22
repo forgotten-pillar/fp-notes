a3040f09a1a96c0293aba204ece78b02a32b5074
mpresecan <m_presecan@yahoo.com>
2024-11-22 18:57:45 +0100

    Quartz sync: Nov 22, 2024, 6:57 PM

--- quartz/components/TTS.tsx
+++ quartz/components/TTS.tsx
@@ -18,6 +18,8 @@ const TTS: QuartzComponent = ({ fileData, displayClass }: QuartzComponentProps) 
             data-publicuserid="433d229fa1db09ffe64719ac56b9de8d83b026ef56baec5e177d3675d7fd6057"
             data-playerurl="https://elevenlabs.io/player/index.html"
             data-small="True"
+            data-backgroundcolor="#ffffff"
+            data-color="#000000"
         >
             Loading the Audio Player... Please reload the page...
         </div>
