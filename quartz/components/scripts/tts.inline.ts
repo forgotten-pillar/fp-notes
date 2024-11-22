9898bad1732d52f2fa1f6106dfc87f8025eb57ae
mpresecan <m_presecan@yahoo.com>
2024-11-22 21:00:43 +0100

    Quartz sync: Nov 22, 2024, 9:00 PM

--- quartz/components/scripts/tts.inline.ts
+++ quartz/components/scripts/tts.inline.ts
@@ -156,27 +156,6 @@ function replaceWidgetTagWithIframe(): void {
 
 
   // fixing SPA
-//   function observeSlugChanges() {
-//     const body = document.body;
-  
-//     // Ensure the <body> exists and has the data-slug attribute
-//     if (body && body.hasAttribute("data-slug")) {
-//       const observer = new MutationObserver((mutationsList) => {
-//         for (const mutation of mutationsList) {
-//           if (
-//             mutation.type === "attributes" &&
-//             mutation.attributeName === "data-slug"
-//           ) {
-//             // Call the function when the slug changes
-//             replaceWidgetTagWithIframe();
-//           }
-//         }
-//       });
-  
-//       // Observe changes to the attributes of <body>
-//       observer.observe(body, { attributes: true });
-//     }
-//   }
-  
-//   // Initialize the observer
-//   observeSlugChanges();
+  document.addEventListener("nav", () => {
+    replaceWidgetTagWithIframe();
+  });
