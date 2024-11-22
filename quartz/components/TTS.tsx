6f2c8da7efc1efec00d0ca3d052d69e228fa5bcd
mpresecan <m_presecan@yahoo.com>
2024-11-22 19:04:22 +0100

    Quartz sync: Nov 22, 2024, 7:04 PM

--- quartz/components/TTS.tsx
+++ quartz/components/TTS.tsx
@@ -6,6 +6,8 @@ import { classNames } from "../util/lang"
 const TTS: QuartzComponent = ({ fileData, displayClass }: QuartzComponentProps) => {
   const tts = fileData.frontmatter?.tts
   
+  const lightTheme = document.documentElement.getAttribute('saved-theme') !== 'dark';
+
   if (tts) {
     return (
       <div id="tts" class={classNames(displayClass, "tts")}>
@@ -18,8 +20,8 @@ const TTS: QuartzComponent = ({ fileData, displayClass }: QuartzComponentProps) 
             data-publicuserid="433d229fa1db09ffe64719ac56b9de8d83b026ef56baec5e177d3675d7fd6057"
             data-playerurl="https://elevenlabs.io/player/index.html"
             data-small="True"
-            data-backgroundcolor="transparent"
-            data-color="black"
+            data-backgroundcolor={lightTheme ? '#fff' : '#1d2535'}
+            data-color={lightTheme ? '#106a8f' : '#ebebec'}
         >
             Loading the Audio Player... Please reload the page...
         </div>
