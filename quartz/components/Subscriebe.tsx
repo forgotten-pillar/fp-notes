ab02964b9f482ab9730500c990ba3ed704eb181c
mpresecan <m_presecan@yahoo.com>
2026-05-17 21:09:23 +0200

    Quartz sync: May 17, 2026, 9:09 PM

--- quartz/components/Subscriebe.tsx
+++ quartz/components/Subscriebe.tsx
@@ -57,8 +57,8 @@ const Subscriebe: QuartzComponent = ({ cfg }: QuartzComponentProps) => {
           </svg>
         </span>
       </button>
-      <p class="notice">
-        Click on the bell icon, and enable push notification, or get notified via email,<br/> or by{" "}
+      <p class="notice subscribe">
+        Click on the bell icon, and enable push notification, or get notified via email, or by{" "}
         <a
           href="https://t.me/forgottenpillar"
           target="_blank"
