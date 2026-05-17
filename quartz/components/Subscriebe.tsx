be9f73f077eb88f60cf3196be90767162143e835
mpresecan <m_presecan@yahoo.com>
2026-05-17 11:30:39 +0200

    feat(subscribe): add bell icon to subscribe component with responsive st
    Agent-Id: agent-013b0a98-40f4-419e-abf2-b12576ac6c88
    Linked-Note-Id: 75c7d70b-2830-424f-a819-1e6f415afaf4

--- quartz/components/Subscriebe.tsx
+++ quartz/components/Subscriebe.tsx
@@ -13,6 +13,24 @@ const Subscriebe: QuartzComponent = ({ cfg }: QuartzComponentProps) => {
       data-push-unsubscribe-url={push?.unsubscribeUrl ?? ""}
     >
       <h3>Get notified when new stuff drops.</h3>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="28"
+        height="28"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="subscribe-bell-icon"
+        aria-hidden="true"
+      >
+        <path d="M10.268 21a2 2 0 0 0 3.464 0" />
+        <path d="M22 8c0-2.3-.8-4.3-2-6" />
+        <path d="M3.262 15.326A1 1 0 0 0 4 17h16a1 1 0 0 0 .74-1.673C19.41 13.956 18 12.499 18 8A6 6 0 0 0 6 8c0 4.499-1.411 5.956-2.738 7.326" />
+        <path d="M4 2C2.8 3.7 2 5.7 2 8" />
+      </svg>
       <p class="notice">
         Get notified via email, or by{" "}
         <a
